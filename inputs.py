5665ef8c3f18ad0993f1658e57a7800faf9be0ae
Pier Paolo Pittavino <pierpa@analisi>
2011-09-02 12:40:22 +0200

    MINIMA INTERFACCIA GRAFICA
    RICHIAMA L'OGGETTO inputs.VideoInput per creare nuove sorgenti video

---
+++ inputs.py
@@ -0,0 +1,237 @@
+'''By Pierpa and Packs: This class
+add a viewer for a video input in the main 
+program window
+'''
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import pygst
+pygst.require("0.10")
+import gst
+
+
+class VideoInput(gtk.Window):
+    '''This class create a viewers with its own toolbar and its own gtk.Window
+	for a gstreamer video pipeline who could be imported in other windows 
+	using reparenting:
+
+	import inputs
+	(...)
+	viewer = inputs.VideoInput() 
+	viewer.set_pipeline(pipeline)
+	childWidget = viewer.main_vbox
+	childWidget.reparent(self.main_vbox)
+	childWidget.show_all()
+    '''	
+	
+    def __init__(self):
+	self.status="PAUSE"
+	self.label=""
+        gtk.Window.__init__(self)
+    	self.set_title(self.get_label())
+        self.connect('delete-event', self._on_delete_event)
+        self.set_position(gtk.WIN_POS_CENTER)
+        self.set_size_request(200, 200)
+	self.ui_string = """
+		<ui>
+  		  <toolbar name='Toolbar'>
+    		    <toolitem action='Play'/>
+    		    <toolitem action='Pause'/>
+    		    <toolitem action='Rec'/>
+    		    <separator/>
+    		    <toolitem action='Remove'/>
+  		  </toolbar>
+		</ui>"""
+	uimgr = self._create_ui()
+        uimgr.connect('connect-proxy',
+                      self._on_uimanager__connect_proxy)
+        uimgr.connect('disconnect-proxy',
+                      self._on_uimanager__disconnect_proxy)
+        toolbar = uimgr.get_widget('/Toolbar')
+        label = gtk.Label(self.get_label())
+	da = gtk.DrawingArea()
+	vbox=gtk.VBox()
+	vbox.pack_start(label,expand=False)
+	vbox.add(da)
+	vbox.pack_end(toolbar,expand=False)
+	self.main_vbox=vbox
+	self.da=da
+	self.add(vbox)
+
+    def _on_uimanager__connect_proxy(self, uimgr, action, widget):
+        tooltip = action.get_property('tooltip')
+        if not tooltip:
+            return
+
+        if isinstance(widget, gtk.MenuItem):
+            cid = widget.connect('select', self._on_menu_item__select,
+                                 tooltip)
+            cid2 = widget.connect('deselect', self._on_menu_item__deselect)
+            widget.set_data('pygtk-app::proxy-signal-ids', (cid, cid2))
+        #elif isinstance(widget, gtk.ToolButton):
+            #cid = widget.child.connect('enter', self._on_tool_button__enter,
+            #                           tooltip)
+            #cid2 = widget.child.connect('leave', self._on_tool_button__leave)
+            #widget.set_data('pygtk-app::proxy-signal-ids', (cid, cid2))
+
+    def _on_uimanager__disconnect_proxy(self, uimgr, action, widget):
+        cids = widget.get_data('pygtk-app::proxy-signal-ids')
+        if not cids:
+            return
+
+        if isinstance(widget, gtk.ToolButton):
+            widget = widget.child
+
+        for name, cid in cids:
+            widget.disconnect(cid)
+
+    #def _on_tool_button__enter(self, toolbutton, tooltip):
+    #    #self.statusbar.push(self._menu_cix, tooltip)
+    #	raise
+
+    #def _on_tool_button__leave(self, toolbutton):
+    #    #self.statusbar.pop(self._menu_cix)
+    #    raise
+
+    def _on_delete_event(self,window,event):
+        gtk.main_quit()
+
+    def _create_ui(self):
+        ag = gtk.ActionGroup('AppActions')
+        actions = [
+            ('Play',     gtk.STOCK_MEDIA_PLAY, '_Play', '',
+             'Playing this video input', self._on_action_play),
+            ('Pause',     gtk.STOCK_MEDIA_PAUSE, '_Pause', '',
+             'Pause playing this video input', self._on_action_pause),
+            ('Rec',     gtk.STOCK_MEDIA_RECORD, '_Rec', '',
+             'Record this video input', self._on_action_rec),
+            ('Remove',     gtk.STOCK_QUIT, '_Remove', '',
+             'Remove this video input', self._on_action_remove)
+            ]
+        ag.add_actions(actions)
+        ui = gtk.UIManager()
+        ui.insert_action_group(ag, 0)
+        ui.add_ui_from_string(self.ui_string)
+        self.add_accel_group(ui.get_accel_group())
+        return ui
+
+    def set_pipeline(self,pipeline):
+	self.pipeline=pipeline
+	self._pipeline()	
+
+    def get_pipeline(self):
+	return self.pipeline
+
+    def set_label(self,label):
+	self.label=label	
+
+    def get_label(self):
+	if not self.label:
+		self.set_label("New video input")
+	return self.label
+
+    def _pipeline(self):
+	"""
+	This method create a pipeline as described in "pipeline"
+	and redirect video output into the DrawingArea() passed
+	as second argument.
+
+	The callback are created at runtime using python closure.
+	"""
+	
+	def __cb_on_sync():
+		def on_sync_message(bus, message):
+			if message.structure is None:
+				return
+			message_name = message.structure.get_name()
+			#print '###', message_name
+
+			"""
+			following this FAQ 
+
+			http://faq.pygtk.org/index.py?file=faq20.006.htp&req=show
+			
+			we have to wrap code touching widgets
+			with gtk.gdk.threads_enter() and gtk.gdk.threads_leave() otherwise
+			errors like 
+
+			python: ../../src/xcb_io.c:221: poll_for_event: asserzione "(((long) (event_sequence) - (long) (dpy->request)) <= 0)" non riuscita.
+
+			will appear.
+			"""
+			gtk.gdk.threads_enter()
+			if message_name == "prepare-xwindow-id":
+				# Assign the viewport
+				imagesink = message.src
+				imagesink.set_property("force-aspect-ratio", True)
+				imagesink.set_xwindow_id(self.da.window.xid)
+
+			gtk.gdk.threads_leave()
+
+		return on_sync_message
+
+	def __cb_factory(p):
+		def _cb(bus, message):
+			t = message.type
+			if t == gst.MESSAGE_EOS:
+				p.set_state(gst.STATE_NULL)
+				self.status="PLAY"
+			elif t == gst.MESSAGE_ERROR:
+				err, debug = message.parse_error()
+				print "Error: %s" % err, debug
+				p.set_state(gst.STATE_NULL)
+				self.status="PLAY"
+		return _cb
+
+	player = gst.parse_launch(self.get_pipeline())
+	bus = player.get_bus()
+	bus.add_signal_watch()
+	bus.enable_sync_message_emission()
+	bus.connect("message", __cb_factory(player))
+	bus.connect("sync-message::element", __cb_on_sync())
+	
+	self.player=player
+
+    def _on_action_play(self, action):
+        self.play()
+
+    def _on_action_pause(self, action):
+        self.pause()
+
+    def _on_action_rec(self, action):
+        self.rec()
+
+    def _on_action_remove(self, action):
+        self.remove()
+
+    def play(self):
+	self.status="PLAY"
+	state = gst.STATE_PLAYING
+	self.player.set_state(state)
+
+    def pause(self):
+	self.status="PAUSE"
+	state = gst.STATE_NULL
+	self.player.set_state(state)
+
+    def rec(self):
+	print "REC function not yet implemented"
+
+    def remove(self):
+	self.destroy()
+	print "REMOVE function not yet implemented"
+
+    def show(self):
+	self.show_all()
+
+    def run(self):	
+	self.show()
+
+if __name__ == '__main__':
+    
+    b = VideoInput()
+    b.set_label("Test on /dev/video0")
+    b.set_pipeline("v4l2src device=/dev/video0 ! autovideosink")
+    b.run()	
+    gtk.main()
--- streamstudio.py
+++ streamstudio.py
@@ -1,141 +1,348 @@
 #!/usr/bin/env python
+'''By Pierpa and Packs: We are trying to 
+build a studio for lots of video/audio 
+input who generate a virtual webcam as output '''
 
-import sys, os
-import pygtk, gtk, gobject
+import pygtk
+pygtk.require('2.0')
+import gtk
 import pygst
 pygst.require("0.10")
 import gst
-import gtk.glade
+import inputs
 
-class GTK_Main:
 
-	def __init__(self):
-		#self.wTree = gtk.glade.XML("streamstudio_gui.glade", "mainwindow")
-		#
-		#signals = {
-		#	"on_play_clicked" : self.start_stop
-		#}
-		#self.wTree.signal_autoconnect(signals)
+ui_string = """<ui>
+  <menubar name='Menubar'>
+    <menu action='FileMenu'>
+      <menuitem action='Add a webcam'/>
+      <menuitem action='Add a gstreamer pipeline'/>
+      <menuitem action='Open'/>
+      <menuitem action='Save'/>
+      <separator/>
+      <menuitem action='Quit'/>
+    </menu>
+    <menu action='HelpMenu'>
+      <menuitem action='About'/>
+    </menu>
+  </menubar>
+  <toolbar name='Toolbar'>
+    <toolitem action='Add a webcam'/>
+    <toolitem action='Add a gstreamer pipeline'/>
+    <toolitem action='Open'/>
+    <toolitem action='Save'/>
+    <separator/>
+    <toolitem action='Quit'/>
+  </toolbar>
+</ui>"""
 
-		window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		window.set_title("Webcam-Viewer")
-		window.set_default_size(800, 900)
-		window.connect("destroy", gtk.main_quit, "WM destroy")
-		vbox = gtk.VBox()
-		window.add(vbox)
-		self.movie_window = gtk.DrawingArea()
-		self.movie_window2 = gtk.DrawingArea()
-		self.movie_window3 = gtk.DrawingArea()
-		vbox.add(self.movie_window)
-		vbox.pack_start(self.movie_window)
-		vbox.pack_start(self.movie_window2)
-		vbox.pack_start(self.movie_window3)
-		hbox = gtk.HBox()
-		vbox.pack_start(hbox, False)
-		hbox.set_border_width(10)
-		hbox.pack_start(gtk.Label())
-		self.button = gtk.Button("Start")
-		self.button.connect("clicked", self.start_stop)
-		hbox.pack_start(self.button, False)
-		self.button2 = gtk.Button("Quit")
-		self.button2.connect("clicked", self.exit)
-		hbox.pack_start(self.button2, False)
-		hbox.add(gtk.Label())
-		window.show_all()
-                #
-		# Set up the gstreamer pipeline
-		self.player = gst.parse_launch ("v4l2src ! autovideosink")
-		self.player = gst.parse_launch ("v4l2src device=/dev/video2 ! autovideosink")
-		self.player2 = gst.parse_launch ("souphttpsrc location=http://90.147.27.235:8080/videofeed ! jpegdec ! autovideosink")
-		self.player3 = gst.parse_launch ("v4l2src device=/dev/video0 ! autovideosink")
-                
-		bus = self.player.get_bus()
-		bus2 = self.player2.get_bus()
-		bus3 = self.player3.get_bus()
-		bus.add_signal_watch()
-		bus2.add_signal_watch()
-		bus3.add_signal_watch()
-		bus.enable_sync_message_emission()
-		bus2.enable_sync_message_emission()
-		bus3.enable_sync_message_emission()
-		bus.connect("message", self.on_message)
-		bus2.connect("message", self.on_message2)
-		bus3.connect("message", self.on_message3)
-		bus.connect("sync-message::element", self.on_sync_message)
-		bus2.connect("sync-message::element", self.on_sync_message2)
-		bus3.connect("sync-message::element", self.on_sync_message3)
 
-	def start_stop(self, w):
-		if self.button.get_label() == "Start":
-			self.button.set_label("Stop")
-			self.player.set_state(gst.STATE_PLAYING)
-			self.player2.set_state(gst.STATE_PLAYING)
-			self.player3.set_state(gst.STATE_PLAYING)
+
+class StreamStudio(gtk.Window):
+    def __init__(self, title='StreamStudio'):
+	self.pipelines=[] #active pipelines 
+        gtk.Window.__init__(self)
+        self.connect('delete-event', self._on_delete_event)
+        self.set_position(gtk.WIN_POS_CENTER)
+        self.set_size_request(600, 400)
+        self.set_title(title)
+
+        main_vbox = gtk.VBox()
+	self.main_vbox=main_vbox
+        self.add(main_vbox)
+        main_vbox.show()
+
+        uimgr = self._create_ui()
+        uimgr.connect('connect-proxy',
+                      self._on_uimanager__connect_proxy)
+        uimgr.connect('disconnect-proxy',
+                      self._on_uimanager__disconnect_proxy)
+
+        menubar = uimgr.get_widget('/Menubar')
+        main_vbox.pack_start(menubar, expand=False)
+        menubar.show()
+
+        toolbar = uimgr.get_widget('/Toolbar')
+        main_vbox.pack_start(toolbar, expand=False)
+        toolbar.realize()
+        toolbar.show()
+	
+	viewers_pane=gtk.HPaned()
+	sources_vbox = gtk.VBox()
+	output_vbox  = gtk.VBox()
+	viewers_pane.add1(sources_vbox)
+	viewers_pane.add2(output_vbox)
+	self.sources_vbox = sources_vbox
+	main_vbox.add(viewers_pane)
+	viewers_pane.show_all()
+
+        status = gtk.Statusbar()
+        main_vbox.pack_end(status, expand=False)
+        status.show()
+        self.statusbar = status
+
+        self._menu_cix = -1
+
+    def _create_ui(self):
+        ag = gtk.ActionGroup('AppActions')
+        actions = [
+            ('FileMenu', None, '_File'),
+            ('Add a webcam',      gtk.STOCK_NEW, '_Add_a_webcam', '<control>N',
+             'Add a new webcam', self._on_action_new),
+            ('Add a gstreamer pipeline',      gtk.STOCK_DISCONNECT, '_Add_a_gstreamer_pipeline', '<control>G',
+             'Add a new, alreasy tested, gstreamer pipeline', self._on_action_new_gs_pipeline),
+            ('Open',     gtk.STOCK_OPEN, '_Open', '<control>O',
+             'Open a configuration file', self._on_action_open),
+            ('Save',     gtk.STOCK_SAVE, '_Save', '<control>S',
+             'Save current configuration in a file', self._on_action_save),
+            ('Quit',     gtk.STOCK_QUIT, '_Quit', '<control>Q',
+             'Quit application', self._on_action_quit),
+            ('HelpMenu', None, '_Help'),
+            ('About',    None, '_About', None, 'About application',
+             self._on_action_about),
+            ]
+        ag.add_actions(actions)
+        ui = gtk.UIManager()
+        ui.insert_action_group(ag, 0)
+        ui.add_ui_from_string(ui_string)
+        self.add_accel_group(ui.get_accel_group())
+        return ui
+
+    def _on_uimanager__connect_proxy(self, uimgr, action, widget):
+        tooltip = action.get_property('tooltip')
+        if not tooltip:
+            return
+
+        if isinstance(widget, gtk.MenuItem):
+            cid = widget.connect('select', self._on_menu_item__select,
+                                 tooltip)
+            cid2 = widget.connect('deselect', self._on_menu_item__deselect)
+            widget.set_data('pygtk-app::proxy-signal-ids', (cid, cid2))
+        elif isinstance(widget, gtk.ToolButton):
+            cid = widget.child.connect('enter', self._on_tool_button__enter,
+                                       tooltip)
+            cid2 = widget.child.connect('leave', self._on_tool_button__leave)
+            widget.set_data('pygtk-app::proxy-signal-ids', (cid, cid2))
+
+    def _on_uimanager__disconnect_proxy(self, uimgr, action, widget):
+        cids = widget.get_data('pygtk-app::proxy-signal-ids')
+        if not cids:
+            return
+
+        if isinstance(widget, gtk.ToolButton):
+            widget = widget.child
+
+        for name, cid in cids:
+            widget.disconnect(cid)
+
+    def _on_menu_item__select(self, menuitem, tooltip):
+        self.statusbar.push(self._menu_cix, tooltip)
+
+    def _on_menu_item__deselect(self, menuitem):
+        self.statusbar.pop(self._menu_cix)
+
+    def _on_tool_button__enter(self, toolbutton, tooltip):
+        self.statusbar.push(self._menu_cix, tooltip)
+
+    def _on_tool_button__leave(self, toolbutton):
+        self.statusbar.pop(self._menu_cix)
+
+    def _on_action_new(self, action):
+        self.new()
+    
+    def _on_action_new_gs_pipeline(self, action):
+        self.new_gs_pipeline()
+
+    def _on_action_open(self, action):
+        self.open()
+
+
+    def _on_action_save(self, action):
+        self.save()
+
+    def _on_action_quit(self, action):
+        self.quit()
+
+    def _on_action_about(self, action):
+	self.about()	
+    
+    def _on_delete_event(self, window, event):
+        self.quit()
+
+    def _control_pipeline(self, pipeline):
+	"""Control if pipeline is already present in
+	   self.pipelines, else add it and create a new VideInput object
+	"""
+
+	if pipeline not in self.pipelines:
+		self.pipelines.append(pipeline)
+		self._add_viewer(pipeline)
+	else:
+		self._alert_message("(%s) pipeline already present" % pipeline)
+	
+    def _on_device_selection(self, filename):
+	"""
+	When the selection is successfully then get the filename
+	create the proper pipeline and pass it to self._control_pipeline. 
+	"""
+	pipeline="v4l2src device=%s ! autovideosink" % filename	
+	self._control_pipeline(pipeline)
+    
+    def _alert_message(self, message):
+	self.statusbar.push(self._menu_cix,message)
+	print message
+
+    def _add_viewer(self,pipeline):
+	viewer = inputs.VideoInput() 
+	viewer.set_pipeline(pipeline)
+	childWidget = viewer.main_vbox
+	childWidget.reparent(self.sources_vbox)
+	childWidget.show_all()
+		
+    # Override in subclass
+	
+    def new(self):
+	"""Open a dialog to choose the proper video device
+	   then pass chosed file to self._on_device_selection
+	"""
+	
+	fs = gtk.FileChooserDialog("Choose a video device",None,
+				    gtk.FILE_CHOOSER_ACTION_OPEN,
+				    (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+                                    gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+	fs.set_default_response(gtk.RESPONSE_OK)
+	filter = gtk.FileFilter()
+	filter.set_name("Video devices")
+	filter.add_pattern("video*")
+	fs.add_filter(filter)
+	filter = gtk.FileFilter()
+	filter.set_name("All files")
+	filter.add_pattern("*")
+	fs.add_filter(filter)
+	fs.set_current_folder("/dev")
+	fs.add_shortcut_folder("/dev")
+	response = fs.run()
+	if response == gtk.RESPONSE_OK:
+    	   self._on_device_selection(fs.get_filename())
+	elif response == gtk.RESPONSE_CANCEL:
+   	   self._alert_message('No video device selected')
+	fs.destroy()
+
+
+    def new_gs_pipeline(self):
+	"""Opens a dialog window to insert your own gstreamer pipeline"""
+        label = gtk.Label("Insert your already tested gstreamer pipeline:")
+        dialog = gtk.Dialog("Add a Gstreamer pipeline",
+                   	None,
+                  	 gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                   	(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                    	gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+       	dialog.vbox.pack_start(label)
+       	label.show()
+	entry = gtk.Entry()
+	dialog.vbox.pack_start(entry)
+	entry.show()
+       	response = dialog.run()
+	if response == -3:
+	   gspipeline=entry.get_text()
+	   if gspipeline:
+		self._control_pipeline(gspipeline)
+       	dialog.destroy()
+ 
+    def open(self):
+	fs = gtk.FileChooserDialog("Choose a file with pipelines",None,
+				    gtk.FILE_CHOOSER_ACTION_OPEN,
+				    (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+                                    gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+	fs.set_default_response(gtk.RESPONSE_OK)
+	filter = gtk.FileFilter()
+	filter.set_name("StreamStudio files")
+	filter.add_pattern("*.streamstudio")
+	fs.add_filter(filter)
+	filter = gtk.FileFilter()
+	filter.set_name("All files")
+	filter.add_pattern("*")
+	fs.add_filter(filter)
+	#fs.set_current_folder("$HOME/.streamstudio")
+	response = fs.run()
+	if response == gtk.RESPONSE_OK:
+   	   filename=fs.get_filename()
+	   if not filename.split(".")[-1] == "streamstudio":
+		filename += ".streamstudio"
+	   f=open(filename,'r')
+	   for pipeline in f.readlines():
+		pipeline=pipeline[:-1]
+		if pipeline not in self.pipelines:
+			self._add_viewer(pipeline)
 		else:
-			self.player.set_state(gst.STATE_NULL)
-			self.player2.set_state(gst.STATE_NULL)
-			self.player3.set_state(gst.STATE_NULL)
-			self.button.set_label("Start")
+			self._alert_message("(%s) pipeline already active" % pipeline)
+	   f.close()
+	elif response == gtk.RESPONSE_CANCEL:
+   	   self._alert_message('pipelines\' configuration file opening aborted')
+	fs.destroy()
 
-	def exit(self, widget, data=None):
-		gtk.main_quit()
 
-	def on_message(self, bus, message):
-		t = message.type
-		if t == gst.MESSAGE_EOS:
-			self.player.set_state(gst.STATE_NULL)
-			self.button.set_label("Start")
-		elif t == gst.MESSAGE_ERROR:
-			err, debug = message.parse_error()
-			print "Error: %s" % err, debug
-			self.player.set_state(gst.STATE_NULL)
-			self.button.set_label("Start")
-	def on_message2(self, bus, message):
-		t = message.type
-		if t == gst.MESSAGE_EOS:
-			self.player2.set_state(gst.STATE_NULL)
-		elif t == gst.MESSAGE_ERROR:
-			err, debug = message.parse_error()
-			print "Error: %s" % err, debug
-			self.player2.set_state(gst.STATE_NULL)
-	def on_message3(self, bus, message):
-		t = message.type
-		if t == gst.MESSAGE_EOS:
-			self.player3.set_state(gst.STATE_NULL)
-		elif t == gst.MESSAGE_ERROR:
-			err, debug = message.parse_error()
-			print "Error: %s" % err, debug
-			self.player3.set_state(gst.STATE_NULL)
+    def save(self):
+	if not len(self.pipelines):
+		
+        	dialog = gtk.MessageDialog(self,
+                                   (gtk.DIALOG_MODAL |
+                                    gtk.DIALOG_DESTROY_WITH_PARENT),
+                                   gtk.MESSAGE_INFO, gtk.BUTTONS_OK,
+                                   "Choose at least one webcam or add a gstreamer pipeline before saving!")
+        	dialog.run()
+        	dialog.destroy()
+		return
+	fs = gtk.FileChooserDialog("Choose a file to save active pipelines",None,
+				    gtk.FILE_CHOOSER_ACTION_SAVE,
+				    (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+                                    gtk.STOCK_SAVE, gtk.RESPONSE_OK))
+	fs.set_default_response(gtk.RESPONSE_OK)
+	filter = gtk.FileFilter()
+	filter.set_name("StreamStudio files")
+	filter.add_pattern("*.streamstudio")
+	fs.add_filter(filter)
+	filter = gtk.FileFilter()
+	filter.set_name("All files")
+	filter.add_pattern("*")
+	fs.add_filter(filter)
+	#fs.set_current_folder("$HOME/.streamstudio")
+	fs.set_do_overwrite_confirmation(True)
+	response = fs.run()
+	if response == gtk.RESPONSE_OK:
+   	   filename=fs.get_filename()
+	   #BUG PRESENT HERE: if you write an existing filename without extension
+	   #it overwrites file without asking confirmation
+	   if not filename.split(".")[-1] == "streamstudio":
+		filename += ".streamstudio"
+	   f=open(filename,'w')
+	   for pipeline in self.pipelines:
+	   	f.writelines(pipeline+"\n")
+	   f.close()
+	elif response == gtk.RESPONSE_CANCEL:
+   	   _alert_message('pipelines\' saving aborted')
+	fs.destroy()
 
-	def on_sync_message(self, bus, message):
-		if message.structure is None:
-			return
-		message_name = message.structure.get_name()
-		if message_name == "prepare-xwindow-id":
-			# Assign the viewport
-			imagesink = message.src
-			imagesink.set_property("force-aspect-ratio", True)
-			imagesink.set_xwindow_id(self.movie_window.window.xid)
-	def on_sync_message2(self, bus, message):
-		if message.structure is None:
-			return
-		message_name = message.structure.get_name()
-		if message_name == "prepare-xwindow-id":
-			# Assign the viewport
-			imagesink = message.src
-			imagesink.set_property("force-aspect-ratio", True)
-			imagesink.set_xwindow_id(self.movie_window2.window.xid)
-	def on_sync_message3(self, bus, message):
-		if message.structure is None:
-			return
-		message_name = message.structure.get_name()
-		if message_name == "prepare-xwindow-id":
-			# Assign the viewport
-			imagesink = message.src
-			imagesink.set_property("force-aspect-ratio", True)
-			imagesink.set_xwindow_id(self.movie_window3.window.xid)
 
-GTK_Main()
-gtk.gdk.threads_init()
-gtk.main()
+
+    def about(self):
+        dialog = gtk.MessageDialog(self,
+                                   (gtk.DIALOG_MODAL |
+                                    gtk.DIALOG_DESTROY_WITH_PARENT),
+                                   gtk.MESSAGE_INFO, gtk.BUTTONS_OK,
+                                   "We are trying to build a studio for lots of video/audio input who generate a virtual webcam as output")
+        dialog.run()
+        dialog.destroy()
+
+    def run(self):
+        self.show()
+	#self._players = []
+        gtk.main()
+
+    def quit(self):
+        gtk.main_quit()
+
+if __name__ == '__main__':
+    a = StreamStudio()
+    a.run()
 
