d0b76474da707f1d9c5c1f584e0387704845915e
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2012-10-23 16:13:52 +0200

    Make log INFO level.

--- sslog.py
+++ sslog.py
@@ -2,10 +2,10 @@ import logging
 
 # FIXME: globals are evils
 stream = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s')
+formatter = logging.Formatter('%(levelname)s - %(filename)s:%(lineno)d - %(message)s')
 
 logger = logging.getLogger(__file__)
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 logger.addHandler(stream)
 stream.setFormatter(formatter)
 
