d4d1838253559e175a01940ca51a79ed302d500b
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-01-16 16:22:54 +0100

    Move streamstudio GUI to glade.

--- streamstudio.py
+++ streamstudio.py
@@ -17,94 +17,30 @@ import sys
 import gobject
 import thread
 from sslog import logger
+from gui import GuiMixin
 
 
-ui_string = """<ui>
-  <menubar name='Menubar'>
-    <menu action='FileMenu'>
-      <menuitem action='Add a webcam'/>
-      <menuitem action='Add a gstreamer pipeline'/>
-      <menuitem action='Open'/>
-      <menuitem action='Save'/>
-      <separator/>
-      <menuitem action='Quit'/>
-    </menu>
-    <menu action='HelpMenu'>
-      <menuitem action='About'/>
-    </menu>
-  </menubar>
-  <toolbar name='Toolbar'>
-    <toolitem action='Add a webcam'/>
-    <toolitem action='Add a gstreamer pipeline'/>
-    <toolitem action='Open'/>
-    <toolitem action='Save'/>
-    <separator/>
-    <toolitem action='Quit'/>
-  </toolbar>
-</ui>"""
-
-class StreamStudio(gtk.Window):
+class StreamStudio(GuiMixin):
+    main_class = 'ssWindow'
     def __init__(self, videodevicepaths, title='StreamStudio'):
-        gtk.Window.__init__(self)
-        self.connect('delete-event', self._on_delete_event)
-        self.set_position(gtk.WIN_POS_CENTER)
-        self.set_size_request(600, 400)
-        self.set_title(title)
-
-        main_vbox = gtk.VBox()
-        self.main_vbox=main_vbox
-        self.add(main_vbox)
-        main_vbox.show()
-
-        uimgr = self._create_ui()
-        uimgr.connect('connect-proxy',
-                      self._on_uimanager__connect_proxy)
-        uimgr.connect('disconnect-proxy',
-                      self._on_uimanager__disconnect_proxy)
-
-        menubar = uimgr.get_widget('/Menubar')
-        main_vbox.pack_start(menubar, expand=False)
-        menubar.show()
-
-        toolbar = uimgr.get_widget('/Toolbar')
-        main_vbox.pack_start(toolbar, expand=False)
-        toolbar.realize()
-        toolbar.show()
-
-        #self.videowidget.connect_after(
-        #   'realize',
-        #   lambda *x: self.main_pipeline_play()
-        #)
-
-        viewers_pane=gtk.HPaned()
-        sources_vbox = gtk.VBox()
-        output_vbox  = gtk.VBox()
-        viewers_pane.add1(sources_vbox)
-        viewers_pane.add2(output_vbox)
-        self.sources_vbox = sources_vbox
-        main_vbox.add(viewers_pane)
-        viewers_pane.show_all()
-
-        status = gtk.Statusbar()
-        main_vbox.pack_end(status, expand=False)
-        status.show()
-        self.statusbar = status
-
-        self._menu_cix = -1
+        self._build_gui()
+        self.main_monitor_widget = self._get_ui_element_by_name('frame1')
+        self.sources_vbox = self._get_ui_element_by_name('box3')
 
         self.videodevicepaths = videodevicepaths
         # this dictionary will have as keys the VideoInput instances
         # and as values the respective video devices.
         self.monitors = {} 
+
         self.videowidget = inputs.VideoInput()
         childWidget = self.videowidget.main_vbox
-        childWidget.reparent(self.sources_vbox)
+        # this is the main monitor so attach it to the left
+        childWidget.reparent(self.main_monitor_widget)
         childWidget.show_all()
 
         # http://blog.yorba.org/jim/2010/10/those-realize-map-widget-signals.html
         # map-event: is a GDK event. This is called when the window is now on-screen,
         #            i.e. the connection is complete. It's like a callback.
-        self.connect("map-event", self._on_show)
 
     def _add_viewer_to_gui(self, devicepath=None):
         """Create a VideoInput attach it to the main GUI and return it.
@@ -165,71 +101,6 @@ class StreamStudio(gtk.Window):
                 logger.exception(e)
         logger.debug("set sink: exit")
 
-    def _create_ui(self):
-        ag = gtk.ActionGroup('AppActions')
-        actions = [
-            ('FileMenu', None, '_File'),
-            ('Add a webcam',      gtk.STOCK_NEW, '_Add_a_webcam', '<control>N',
-             'Add a new video source', self._on_action_add_new_video_source),
-            ('Add a gstreamer pipeline',      gtk.STOCK_DISCONNECT, '_Add_a_gstreamer_pipeline', '<control>G',
-             'Add a new, alreasy tested, gstreamer pipeline', self._on_action_new_gs_pipeline),
-            ('Open',     gtk.STOCK_OPEN, '_Open', '<control>O',
-             'Open a configuration file', self._on_action_open),
-            ('Save',     gtk.STOCK_SAVE, '_Save', '<control>S',
-             'Save current configuration in a file', self._on_action_save),
-            ('Quit',     gtk.STOCK_QUIT, '_Quit', '<control>Q',
-             'Quit application', self._on_action_quit),
-            ('HelpMenu', None, '_Help'),
-            ('About',    None, '_About', None, 'About application',
-             self._on_action_about),
-            ]
-        ag.add_actions(actions)
-        ui = gtk.UIManager()
-        ui.insert_action_group(ag, 0)
-        ui.add_ui_from_string(ui_string)
-        self.add_accel_group(ui.get_accel_group())
-
-        return ui
-
-    def _on_uimanager__connect_proxy(self, uimgr, action, widget):
-        tooltip = action.get_property('tooltip')
-        if not tooltip:
-            return
-
-        if isinstance(widget, gtk.MenuItem):
-            cid = widget.connect('select', self._on_menu_item__select,
-                                 tooltip)
-            cid2 = widget.connect('deselect', self._on_menu_item__deselect)
-            widget.set_data('pygtk-app::proxy-signal-ids', (cid, cid2))
-        elif isinstance(widget, gtk.ToolButton):
-            cid = widget.child.connect('enter', self._on_tool_button__enter,
-                                       tooltip)
-            cid2 = widget.child.connect('leave', self._on_tool_button__leave)
-            widget.set_data('pygtk-app::proxy-signal-ids', (cid, cid2))
-
-    def _on_uimanager__disconnect_proxy(self, uimgr, action, widget):
-        cids = widget.get_data('pygtk-app::proxy-signal-ids')
-        if not cids:
-            return
-
-        if isinstance(widget, gtk.ToolButton):
-            widget = widget.child
-
-        for name, cid in cids:
-            widget.disconnect(cid)
-
-    def _on_menu_item__select(self, menuitem, tooltip):
-        self.statusbar.push(self._menu_cix, tooltip)
-
-    def _on_menu_item__deselect(self, menuitem):
-        self.statusbar.pop(self._menu_cix)
-
-    def _on_tool_button__enter(self, toolbutton, tooltip):
-        self.statusbar.push(self._menu_cix, tooltip)
-
-    def _on_tool_button__leave(self, toolbutton):
-        self.statusbar.pop(self._menu_cix)
-
     def _on_action_add_new_video_source(self, action):
         self.add_video_source()
     
@@ -269,7 +140,7 @@ class StreamStudio(gtk.Window):
         self.statusbar.push(self._menu_cix,message)
         print message
 
-    def add_video_source(self):
+    def add_video_source(self, data):
         """Open a dialog to choose the proper video device
            then pass chosed file to self._on_device_selection
         """
@@ -411,9 +282,6 @@ def usage(progname):
 
 if __name__ == '__main__':
     import glib
-    gobject.type_register(StreamStudio)
-    gobject.type_register(inputs.VideoInput)
-
     gobject.threads_init()
     gtk.gdk.threads_init()
     a = StreamStudio(sys.argv[1:])
