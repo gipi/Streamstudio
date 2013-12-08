2e03d951612c11f63fa31c5a0defa08245747036
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-12-08 16:30:09 +0100

    Convert volume-meter to gst1.0.

--- volume.py
+++ volume.py
@@ -1,27 +1,39 @@
 """
 Simple script that take an audio source and show its
 channel levels using a GTK widget.
+
+For RMS/Peak/Decay configuration
+    http://q-syshelp.qschome.com/Content/Schematic%20Library/meter2.htm
 """
-import pygst
-import gst
-import gtk
-import gtk.glade
+from gi.repository import Gst, Gtk
+import math
 
 from gui import GuiMixin
 
 class VolumeWidget(GuiMixin):
     main_class = 'mainWindow'
     glade_file_path = 'volume-meter.glade'
-    def __init__(self):
+
+    def __init__(self, audio_element=None):
         self._build_gui()
+
+        self.gui_volume = self.builder.get_object('volumebutton')
+        self.gui_level = self.builder.get_object('progressbar')
+
         self._create_pipeline()
+
+        # set volume
+        volume = self.pipeline.get_by_name('v')
+        self.set_gui_volume(volume.get_property('volume'))
+
+        self.level = self.pipeline.get_by_name('l')
 
     def _create_pipeline(self):
         """Create a simple pipeline with an audio test source having a level
         and volume element
         """
-        pipestr = 'audiotestsrc wave=ticks ! audioconvert ! level ! volume name=v ! pulsesink'
-        self.pipeline = gst.parse_launch(pipestr)
+        pipestr = 'pulsesrc ! audioconvert ! level name=l ! volume name=v ! pulsesink'
+        self.pipeline = Gst.parse_launch(pipestr)
 
         # get the bus and connect to it some callback
         # in order to get the levels
@@ -32,42 +44,67 @@ class VolumeWidget(GuiMixin):
 
     def on_message(self, bus, message):
         t = message.type
-        if t == gst.MESSAGE_ERROR:
+        if t == Gst.MessageType.ERROR:
             err, debug = message.parse_error()
             print "Error: %s" % err, debug
             if self.on_eos:
                 self.on_eos()
             self.playing = False
-        elif t == gst.MESSAGE_EOS:
+        elif t == Gst.MessageType.EOS:
             if self.on_eos:
                 self.on_eos()
             self.playing = False
-        elif t == gst.MESSAGE_ELEMENT:
-            message_name = message.structure.get_name()
+        elif t == Gst.MessageType.ELEMENT:
+            message_name = message.get_structure().get_name()
             if message_name == "level":
-                #import pdb;pdb.set_trace()
-                print message.structure["peak"], message.structure["decay"]
+                rms = message.get_structure().get_value('rms')[0]
+                peak = message.get_structure().get_value('peak')[0]
+                decay = message.get_structure().get_value('decay')[0]
+                print rms, peak, decay
+                self.set_gui_level(self._rescale_db_to_level(rms))
+
+    def _rescale_db_to_level(self, db_value):
+        """Since the level is from 0 to 100 but the db levels are(?) in the
+        range -100 to 20, we use this function to shift and rescale
+
+        0/-100 : 100/20
+        """
+        db_min    = -100.0
+        db_max    = 20.0
+        level_min = 0.0
+        level_max = 100.0
+
+        v = level_min + abs(db_value) / (db_max - db_min) * (level_max - level_min)
+
+        print '###', db_value, v
+
+        return v
+
+    def set_gui_volume(self, volume_level):
+        self.gui_volume.set_value(volume_level)
+
+    def set_gui_level(self, value):
+        self.gui_level.set_value(value)
 
     def play(self):
         self.playing = True
-        gst.info("playing player")
-        self.pipeline.set_state(gst.STATE_PLAYING)
-
-        # set volume
-        volume = self.pipeline.get_by_name('v')
-        volume_value = volume.get_property('volume')
-        self.builder.get_object('volumebutton').set_value(volume_value)
+        #Gst.info("playing player")
+        self.pipeline.set_state(Gst.State.PLAYING)
 
 
     def stop(self):
-        self.pipeline.set_state(gst.STATE_NULL)
-        gst.info("stopped player")
+        self.pipeline.set_state(Gst.State.NULL)
+        #gst.info("stopped player")
         self.playing = False
+
+    def _on_action_quit(self, action):
+        self.stop()
+        Gtk.main_quit()
 
 
 if __name__ == "__main__":
-    gtk.gdk.threads_init()
+    Gst.init(None)
     vw = VolumeWidget()
     vw.show_all()
     vw.play()
-    gtk.main()
+    Gtk.main()
