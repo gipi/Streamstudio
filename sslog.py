0420b594a0ce2ebadc00fe19d825098eb787abf0
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-12-18 23:46:06 +0100

    Introduce the environment variable SS_LOG_LEVEL.

--- sslog.py
+++ sslog.py
@@ -5,7 +5,14 @@ stream = logging.StreamHandler()
 formatter = logging.Formatter('%(levelname)s - %(filename)s:%(lineno)d - %(message)s')
 
 logger = logging.getLogger(__file__)
-logger.setLevel(logging.INFO)
+def get_level():
+    from os import environ
+
+    if environ.has_key('SS_LOG_LEVEL'):
+        return getattr(logging, environ.get('SS_LOG_LEVEL'))
+    else:
+        return logging.DEBUG
+logger.setLevel(get_level())
 logger.addHandler(stream)
 stream.setFormatter(formatter)
 
