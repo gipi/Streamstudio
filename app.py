29acce454225bd76752e7939c3074d37fb389900
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-12-20 15:30:58 +0100

    Make the appsink/source example script switch between different sources.

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ class Main:
         self.source_id = None
         self.lock = Lock()
 
-        self.isWhite = False
+        self.isWhite = True
+        self.isStream = True
         self.timestamp = 0
 
         self.pipeline = Gst.parse_launch(pipeline_string)
@@ -23,7 +24,6 @@ class Main:
         self.appsink = self.pipeline.get_by_name('sink')
 
         assert self.appsink, 'appsink element named \'sink\' not found'
-
 
         self.appsink.connect('new-sample', self.on_new_buffer)
         self.appsink.set_property('emit-signals', True)
@@ -44,6 +44,8 @@ class Main:
 
         self.pipeline_out.set_state(Gst.State.PLAYING)
 
+        GObject.timeout_add_seconds(2, self._switch_data_type)
+
     def on_new_buffer(self, appsink):
         with self.lock:
             bffer = appsink.emit('pull-sample')
@@ -56,32 +58,63 @@ class Main:
         if not self.source_id:
             self.source_id = GObject.idle_add(self.push_data, source)
 
-    def push_data(self, source):
-        # http://gstreamer.freedesktop.org/data/doc/gstreamer/head/manual/html/section-data-spoof.html#section-spoof-appsrc
-        # size = 385 * 288 * 2;
-        #bffer = Gst.Buffer.new_allocate(None, size, None)
-        #bffer.memset(0, 0x00 if self.isWhite else 0xff, size)
-        if not self.data:
-            return True
+    def _switch_data_type(self):
+        self.isStream = not self.isStream
+        if not self.isStream:
+            self.isWhite = not self.isWhite
 
-        bffer = self.data.get_buffer().copy()
-        self.isWhite = not self.isWhite
+        logger.debug('switched to stream: %s and white: %s' % (self.isStream, self.isWhite,))
+
+        # recall us again, over and over again dude
+        return True
+
+    def build_white_black_stream(self, width, height, depth):
+        logger.debug('white: %s' % self.isWhite)
+        # http://gstreamer.freedesktop.org/data/doc/gstreamer/head/manual/html/section-data-spoof.html#section-spoof-appsrc
+        size = width * height * depth
+        bffer = Gst.Buffer.new_allocate(None, size, None)
+
+        bffer.memset(0, 0x08 if self.isWhite else 0xf8, size)
+
+        bffer.pts = self.timestamp
+        bffer.duration = Gst.util_uint64_scale_int(1, Gst.SECOND, 30)
+
+        # NOTE: if you remove this line below the stream after the first
+        # switch doesn't re-switch and the stream appears lagging
+        self.timestamp += bffer.duration
+
+        return bffer
+
+    def copy_from_data(self):
+        with self.lock:
+            if not self.data:
+                return None
+
+            bffer = self.data.get_buffer().copy()
+
+            self.data = None
 
         try:
             bffer.pts = self.timestamp
         except ValueError as e:
             logger.error('%d' % self.timestamp)
             g_main_loop.quit()
-        #bffer.duration = Gst.util_uint64_scale_int (1, Gst.SECOND, 2)
 
         self.timestamp += bffer.duration
 
-        result  = source.emit('push-buffer', bffer)
-        self.data = None
-        logger.debug('pushed %s' % bffer)
+        return bffer
+
+    def push_data(self, source):
+        bff = self.copy_from_data() if self.isStream else self.build_white_black_stream(320, 240, 2)
+
+        if bff is None:
+            return True
+
+        result  = source.emit('push-buffer', bff)
+        logger.debug('pushed %s' % bff)
 
         if result != Gst.FlowReturn.OK:
-            logger.debug('error on on_need_data')
+            logger.debug('error on on_need_data: %s' % result)
             g_main_loop.quit()
 
         return True
