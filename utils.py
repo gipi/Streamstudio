9ff0a6deaeccaca9b2ab54e3006998531c3f0d29
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-12-23 13:18:04 +0100

    Add utils module.

---
+++ utils.py
@@ -0,0 +1,27 @@
+from gi.repository import Gst
+import sys
+
+def _ctrl_c_handling(pipeline):
+    """
+    Exit and dump the pipeline graph. Usage example:
+
+    import signal
+
+    signal.signal(signal.SIGINT, ctrl_c_handling)
+    """
+    def __ctrl_c_handling(signal, frame):
+        Gst.debug_bin_to_dot_file_with_ts(pipeline, Gst.DebugGraphDetails.ALL, '-pad')
+        sys.exit(0)
+
+    return __ctrl_c_handling
+
+import collections
+# http://stackoverflow.com/questions/2158395/flatten-an-irregular-list-of-lists-in-python
+def flatten(l):
+    for el in l:
+        if isinstance(el, collections.Iterable) and not isinstance(el, basestring):
+            for sub in flatten(el):
+                yield sub
+        else:
+            yield el
+
