722c662d1445c4a285f1be3342d459def2a3fbca
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-12-24 23:02:44 +0100

    Extend GuiMixin.

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 """Simple class to inizialize its own GUI loading it by a glade file.
 """
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
+from sslog import logger
 class GuiMixin(object):
     """Mixin to be used to manage easily GUI.
 
@@ -13,6 +14,8 @@ class GuiMixin(object):
     """
     main_class = None
     glade_file_path = None
+    main_container_class = None
+    drawing_area_class = None
 
     def _build_gui(self):
         glade_file = '%s.glade' % self.__class__.__name__.lower() if not self.glade_file_path else self.glade_file_path
@@ -34,6 +37,33 @@ class GuiMixin(object):
     def _get_main_class(self):
         return self._get_ui_element_by_name(self.main_class)
 
+    def _get_widget_to_set_sink(self):
+        return self._get_ui_element_by_name(self.drawing_area_class)
+
+    def get_main_container(self):
+        assert self.main_container_class, "The class '%s' has not defined 'main_container_class' value" % self.__class__
+        return self._get_ui_element_by_name(self.main_container_class)
+
     def show_all(self):
         assert self.main_class, "The class has not defined 'main_class' value."
         self._get_main_class().show_all()
+
+    def reparent_in(self, container):
+        self.get_main_container().reparent(container)
+
+        self._get_main_class().destroy()
+
+    def set_sink(self, sink):
+        # see <http://gstreamer.freedesktop.org/data/doc/gstreamer/head/gst-plugins-base-libs/html/gst-plugins-base-libs-gstvideooverlay.html>
+        Gdk.threads_enter()
+        logger.debug('try to set sink with \'%s\'' % (sink,))
+        try:
+            xid = self._get_widget_to_set_sink().get_property('window').get_xid()
+            assert xid
+            sink.set_property("force-aspect-ratio", True)
+            sink.set_window_handle(xid)
+        except Exception, e:
+            logger.exception(e)
+        finally:
+            Gdk.flush()
+            Gdk.threads_leave()
